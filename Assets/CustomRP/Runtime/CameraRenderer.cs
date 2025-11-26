f9a3068bcaecb857731015051bdff3160e037739
zengweihao <zwh86771385@gmail.com>
2025-11-26 22:51:12 +0800

    绘制物体

--- Assets/CustomRP/Runtime/CameraRenderer.cs
+++ Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -6,31 +6,42 @@ using UnityEngine.Rendering;
 public class CameraRenderer
 {
     ScriptableRenderContext context;
-
     Camera camera;
-
     const string bufferName = "Render Camera";
-
     CommandBuffer buffer = new CommandBuffer { name = bufferName };
+    CullingResults cullingResults;
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
     public void Render(ScriptableRenderContext context,Camera camera)
     {
         this.context = context;
         this.camera = camera;
-
+        if (!Cull()) return;
         Setup();
         DrawVisibleGeometry();
         Submit();
     }
     void Setup()
     {
+        //应该先于clear 否则Unity 不能确定当前 RT、视图矩阵等
+        //只能通过 Draw GL 绘制一个全屏 Quad 清理颜色（效率较低）
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
         buffer.BeginSample(bufferName);
         ExecuteBuffer();
-        context.SetupCameraProperties(camera);
     }
     void DrawVisibleGeometry()
     {
         context.DrawSkybox(camera);//直接使用了 SRP 的内部管线 不需要commandbuffer
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(
+            unlitShaderTagId,sortingSettings);
+        var filteringSetting = new FilteringSettings(RenderQueueRange.all);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSetting);
+       
     }
 
     void Submit()
@@ -44,5 +55,15 @@ public class CameraRenderer
     {
         context.ExecuteCommandBuffer(buffer);
         buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if(camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
     }
 }
